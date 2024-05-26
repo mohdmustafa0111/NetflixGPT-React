813b0ebf1aeb4328faa9067d9242060ace98b223
Mustafa <57066099+mohdmustafa0111@users.noreply.github.com>
2024-05-26 23:43:41 +0530

    created Sign In form

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -4,12 +4,29 @@ const Login = () => {
   return (
     <div>
       <Header />
-      <div>
+      <div className="absolute">
         <img
           src="https://assets.nflxext.com/ffe/siteui/vlv3/41c789f0-7df5-4219-94c6-c66fe500590a/3149e5eb-4660-4e3d-9e65-b1e615229c64/IN-en-20240513-popsignuptwoweeks-perspective_alpha_website_large.jpg"
           alt="Logo"
         />
       </div>
+
+      <form className="absolute w-3/12 p-12 bg-black my-24 mx-auto left-0 right-0 text-white bg-opacity-70">
+        <h1 className="font-bold text-xl py-4">Sign In</h1>
+        <input
+          type="email"
+          placeholder="Email"
+          className="p-4 my-4 w-full bg-gray-700 rounded-lg"
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          className="p-4 my-4 w-full bg-gray-700 rounded-lg"
+        />
+        <button className="p-4 my-6 bg-red-700 w-full rounded-lg">
+          Sign In
+        </button>
+      </form>
     </div>
   );
 };
