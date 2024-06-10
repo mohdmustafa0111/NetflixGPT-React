1b9de2dd363493195f0a37a865903016addcb2e4
Mustafa <57066099+mohdmustafa0111@users.noreply.github.com>
2024-06-10 14:46:42 +0530

    Updated Profile

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,11 @@
 import { signOut } from "firebase/auth";
 import { auth } from "../utils/firebase";
 import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const Header = () => {
   const navigate = useNavigate();
+  const user = useSelector((store) => store.user);
 
   const handleSignOut = () => {
     signOut(auth)
@@ -24,16 +26,14 @@ const Header = () => {
         src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
         alt="logo"
       />
-      <div className="flex p-2">
-        <img
-          className="w-11 h-11"
-          src="https://wallpapers.com/images/hd/netflix-profile-pictures-1000-x-1000-qo9h82134t9nv0j0.jpg"
-          alt="usericon"
-        />
-        <button className="font-bold text-white" onClick={handleSignOut}>
-          (Sign Out)
-        </button>
-      </div>
+      {user && (
+        <div className="flex p-2">
+          <img className="w-11 h-11" src={user?.photoURL} alt="usericon" />
+          <button className="font-bold text-white" onClick={handleSignOut}>
+            (Sign Out)
+          </button>
+        </div>
+      )}
     </div>
   );
 };
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -4,15 +4,20 @@ import { checkValidData } from "../utils/validate.js";
 import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
+  updateProfile,
 } from "firebase/auth";
 import { auth } from "../utils/firebase.js";
 import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { addUser } from "../utils/userSlice.js";
 
 const Login = () => {
   const [isSignInForm, setIsSignInForm] = useState(true);
   const [errorMessage, setErrorMessage] = useState(null);
   const navigate = useNavigate();
+  const dispatch = useDispatch();
 
+  const name = useRef(null);
   const email = useRef(null);
   const password = useRef(null);
 
@@ -36,8 +41,28 @@ const Login = () => {
         .then((userCredential) => {
           // Signed up
           const user = userCredential.user;
-          console.log(user);
-          navigate("./browse");
+          updateProfile(user, {
+            displayName: name.current.value,
+            photoURL:
+              "https://images.ctfassets.net/usf1vwtuqyxm/3SQ3X2km8wkQIsQWa02yOY/8801d7055a3e99dae8e60f54bb4b1db8/HarryPotter_WB_F4_HarryPotterMidshot_Promo_080615_Port.jpg?w=914&q=70&fm=jpg",
+          })
+            .then(() => {
+              // Profile updated!
+              const { uid, email, displayName, photoURL } = auth.currentUser;
+              dispatch(
+                addUser({
+                  uid: uid,
+                  email: email,
+                  displayName: displayName,
+                  photoURL: photoURL,
+                })
+              );
+              navigate("/browse");
+            })
+            .catch((error) => {
+              // An error occurred
+              setErrorMessage(error.message);
+            });
         })
         .catch((error) => {
           const errorCode = error.code;
@@ -87,6 +112,7 @@ const Login = () => {
             type="name"
             placeholder="Full Name"
             className="p-4 my-4 w-full bg-gray-700 rounded-lg"
+            ref={name}
           />
         )}
         <input
