b73c0fb757d1e4f764e89e068ff05ca3277d189e
Mustafa <57066099+mohdmustafa0111@users.noreply.github.com>
2024-06-18 21:20:55 +0530

    Created my own Custom Hook

--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -1,25 +1,8 @@
 import Header from "./Header";
-import { API_OPTIONS } from "../utils/constants";
-import { useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { addNowPlayingMovies } from "../utils/moviesSlice";
+import useNowPlayingMovies from "../hooks/useNowPlayingMovies";
 
 const Browse = () => {
-  const dispatch = useDispatch();
-
-  // Fetch the data from TMDB API and update the store
-  const getNowPlayingMovies = async () => {
-    const data = await fetch(
-      "https://api.themoviedb.org/3/movie/now_playing?page=1",
-      API_OPTIONS
-    );
-    const json = await data.json();
-    dispatch(addNowPlayingMovies(json.results));
-  };
-
-  useEffect(() => {
-    getNowPlayingMovies();
-  }, []);
+  useNowPlayingMovies();
 
   return (
     <div>
