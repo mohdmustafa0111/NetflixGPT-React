30d5be02a06366d90e9c9625c78c9c44108e31df
Mustafa <57066099+mohdmustafa0111@users.noreply.github.com>
2024-06-21 23:48:49 +0530

    Created Custom Hook for Trailer Video

--- src/components/VideoBackground.jsx
+++ src/components/VideoBackground.jsx
@@ -1,31 +1,17 @@
-import React, { useEffect } from "react";
-import { API_OPTIONS } from "../utils/constants";
+import React from "react";
+import useMovieTrailer from "../hooks/useMovieTrailer";
+import { useSelector } from "react-redux";
 
 const VideoBackground = ({ movieId }) => {
-  // Fetch trailor video
-  const getMovieVideos = async () => {
-    const data = await fetch(
-      "https://api.themoviedb.org/3/movie/1022789/videos?language=en-US",
-      API_OPTIONS
-    );
-    const json = await data.json();
-    console.log(json);
-
-    const filterData = json.results.filter((video) => video.type === "Trailer");
-    const trailer = filterData.length ? filterData[0] : json.results[0];
-    console.log(trailer);
-  };
-
-  useEffect(() => {
-    getMovieVideos();
-  }, []);
+  const trailerVideo = useSelector((store) => store.movies?.trailerVideo);
+  useMovieTrailer(movieId);
 
   return (
     <div>
       <iframe
         width="560"
         height="315"
-        src={"https://www.youtube.com/embed/" + trailer.key}
+        src={"https://www.youtube.com/embed/" + trailerVideo?.key}
         title="YouTube video player"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
         referrerPolicy="strict-origin-when-cross-origin"
