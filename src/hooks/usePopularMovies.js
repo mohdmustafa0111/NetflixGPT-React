b6adb7dcd098110d15ed38421e974f417cfa39bd
Mustafa <57066099+mohdmustafa0111@users.noreply.github.com>
2024-06-25 22:20:25 +0530

    Fetched Top Rated Movies API and Built Custom Hook for it.

--- src/hooks/usePopularMovies.js
+++ src/hooks/usePopularMovies.js
@@ -4,8 +4,9 @@ import { useDispatch } from "react-redux";
 import { addPopularMovies } from "../utils/moviesSlice";
 
 const usePopularMovies = () => {
-  // Fetch the data from TMDB API and update the store
   const dispatch = useDispatch();
+
+  // Fetch the data from TMDB API and update the store
   const getPopularMovies = async () => {
     const data = await fetch(
       "https://api.themoviedb.org/3/movie/popular?page=1",
--- src/utils/moviesSlice.js
+++ src/utils/moviesSlice.js
@@ -4,23 +4,31 @@ const moviesSlice = createSlice({
   name: "movies",
   initialState: {
     nowPlayingMovies: null,
-    trailerVideo: null,
+    topRatedMovies: null,
     popularMovies: null,
+    trailerVideo: null,
   },
   reducers: {
     addNowPlayingMovies: (state, action) => {
       state.nowPlayingMovies = action.payload;
     },
-    addTrailerVideo: (state, action) => {
-      state.trailerVideo = action.payload;
+    addTopRatedMovies: (state, action) => {
+      state.topRatedMovies = action.payload;
     },
     addPopularMovies: (state, action) => {
       state.popularMovies = action.payload;
     },
+    addTrailerVideo: (state, action) => {
+      state.trailerVideo = action.payload;
+    },
   },
 });
 
-export const { addNowPlayingMovies, addTrailerVideo, addPopularMovies } =
-  moviesSlice.actions;
+export const {
+  addNowPlayingMovies,
+  addTrailerVideo,
+  addPopularMovies,
+  addTopRatedMovies,
+} = moviesSlice.actions;
 
 export default moviesSlice.reducer;
